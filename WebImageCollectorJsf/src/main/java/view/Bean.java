fd67ac4c3febcc17fccb4ce0ca882f95c1218961
sanek94cool <sanekjedi27@list.ru>
2017-10-08 18:06:12 -0700

    Fixes
    Fixed path to imageData.txt
    added sample image for test
    Still, can't make FileServlet able to work

---
+++ WebImageCollectorJsf/src/main/java/view/Bean.java
@@ -0,0 +1,29 @@
+package main.java.view;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * Created by sanek94cool on 28.09.2017.
+ */
+
+@ManagedBean (name = "javaBean")
+@RequestScoped
+public class Bean {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void handleText()
+    {
+        this.text = text.toUpperCase();
+    }
+
+}
